be5c0fbbebc58fcbc86479981efe4079b657c73d
594904020@qq.com <594904020@qq.com>
2017-11-13 23:59:51 +0800

    Tabbar

--- Container.js
+++ Container.js
@@ -9,21 +9,71 @@ import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  Image
 } from 'react-native';
 import {connect} from "react-redux";
 import {allActionsFun} from "./redux/action"; //导入函数生成action
 
-
+import TabNavigator from 'react-native-tab-navigator';
 
 
 
 class Container extends Component<{}> {
+  constructor(props){
+    super(props)
+    this.state={
+      selected:"home"
+    }
+  }
   render() {
     return (
-      <View style={styles.container}>
-         <Text>adw</Text>
-      </View>
+        <TabNavigator>
+            <TabNavigator.Item
+             selected={this.state.selected=="home"}
+             title={"首页"}
+             titleStyle={styles.titleStyle}
+             onPress={()=>{this.setState({selected:"home"})}}
+             renderIcon={()=> <Image style={styles.tabIcon} source={require("./assets/images/index/home.png")}/>}
+             renderSelectedIcon={()=> <Image  style={styles.tabIcon} source={require("./assets/images/index/home_ok.png")}/>}
+            >
+            <View style={styles.container}><Text>dwadaw</Text></View>
+            </TabNavigator.Item>
+
+            <TabNavigator.Item
+                selected={this.state.selected=="message"}
+                title={"消息"}
+                titleStyle={styles.titleStyle}
+                onPress={()=>{this.setState({selected:"message"})}}
+                renderIcon={()=> <Image   style={styles.tabIcon} source={require("./assets/images/index/message.png")}/>}
+                renderSelectedIcon={()=> <Image style={styles.tabIcon} source={require("./assets/images/index/message_ok.png")}/>}
+            >
+              <View style={styles.container}><Text>1132d</Text></View>
+            </TabNavigator.Item>
+
+          <TabNavigator.Item
+              selected={this.state.selected=="order"}
+              title={"订单"}
+              titleStyle={styles.titleStyle}
+              onPress={()=>{this.setState({selected:"order"})}}
+              renderIcon={()=> <Image   style={styles.tabIcon} source={require("./assets/images/index/order.png")}/>}
+              renderSelectedIcon={()=> <Image style={styles.tabIcon} source={require("./assets/images/index/order_ok.png")}/>}
+          >
+            <View style={styles.container}><Text>1132d</Text></View>
+          </TabNavigator.Item>
+
+
+          <TabNavigator.Item
+              selected={this.state.selected=="my"}
+              title={"我的"}
+              titleStyle={styles.titleStyle}
+              onPress={()=>{this.setState({selected:"my"})}}
+              renderIcon={()=> <Image   style={styles.tabIcon} source={require("./assets/images/index/my.png")}/>}
+              renderSelectedIcon={()=> <Image style={styles.tabIcon} source={require("./assets/images/index/my_ok.png")}/>}
+          >
+            <View style={styles.container}><Text>1132d</Text></View>
+          </TabNavigator.Item>
+        </TabNavigator>
     );
   }
 }
@@ -35,16 +85,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  tabIcon:{
+      width:25,
+      height:25
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  titleStyle:{
+    fontSize:12
+  }
+
 });
 
 
