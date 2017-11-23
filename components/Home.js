4daeacd065d0af32ee13507823303160b428f26e
594904020@qq.com <594904020@qq.com>
2017-11-24 00:16:49 +0800

    home

--- components/Home.js
+++ components/Home.js
@@ -16,12 +16,15 @@ import {
 
 import {connect} from "react-redux";
 import  {Calc} from "./common/Calc"
-var ParallaxView = require('react-native-parallax-view');
+import ParallaxView from "./common/ParallaxView"
 class Home extends Component<{}> {
     constructor(props) {
         super(props)
         this.state={
-
+            addressText:"目的地/客栈名称",
+            addressName:"厦门",
+            liveDate:"10月21日-10月22日",
+            peopleNum:"2人"
         }
     }
 
@@ -34,13 +37,61 @@ class Home extends Component<{}> {
                     windowHeight={Calc.getHeight(547)}
                 >
                   <View>
-                      <Text >awdw</Text>
-                      <Text >awdw</Text>
-                      <Text >awdw</Text>
+                      {/*搜索info*/}
+                      <View style={styles.searchInfo}>
+                          {this.renderAddress()}
+                          {this.renderLiveDate()}
+                          {this.renderPeopleNum()}
+                          <View style={{flex:1,height:Calc.getHeight(88),backgroundColor:"#51cdf1",alignItems:"center",justifyContent:"center",borderRadius:6}}>
+                              <Text style={{fontSize:18,color:"#fff"}}>开始</Text>
+                          </View>
+                      </View>
                   </View>
                 </ParallaxView>
             </View>
         );
+    }
+    //目的地
+    renderAddress(){
+        return (
+            <View style={styles.List}>
+                <Image style={styles.smallIcon} source={require("../assets/images/index/addressIcon.png")}/>
+                <Text numberOfLines={1} style={{flex:1,fontSize:15,color:"#b8bdc2"}}>{this.state.addressText}</Text>
+                <Text style={styles.rightText}>{this.state.addressName}</Text>
+                <Image style={styles.enterIcon} source={require("../assets/images/common/enter.png")}/>
+            </View>
+        )
+    }
+    //入住时间
+    renderLiveDate(){
+        return(
+            <View style={styles.List}>
+                <Image style={styles.smallIcon} source={require("../assets/images/index/liveDate.png")}/>
+                <View style={{flex:1,flexDirection:"column",justifyContent:"center"}}>
+                    {/*住房入住*/}
+                    <View style={{flexDirection:"row",marginBottom:Calc.getWidth(10)}}>
+                        <Text style={{fontSize:12,color:"#b8bdc2",marginRight:Calc.getWidth(102)}}>入住</Text>
+                        <Text style={{fontSize:12,color:"#b8bdc2"}}>退房</Text>
+                    </View>
+                    {/*入住日期*/}
+                    <Text numberOfLines={1} style={{fontSize:16,color:"#3a3c3c"}}>{this.state.liveDate}</Text>
+                </View>
+                {/*入住几晚*/}
+                <Text style={styles.rightText}>共一晚</Text>
+                <Image style={styles.enterIcon} source={require("../assets/images/common/enter.png")}/>
+            </View>
+        )
+    }
+    //人数
+    renderPeopleNum(){
+        return(
+            <View style={[styles.List,{borderColor:"#fff"}]}>
+                <Image style={styles.smallIcon} source={require("../assets/images/index/peopleNum.png")}/>
+                <Text numberOfLines={1} style={{flex:1,fontSize:15,color:"#b8bdc2"}}>人数</Text>
+                <Text style={styles.rightText}>{this.state.peopleNum}</Text>
+                <Image style={styles.enterIcon} source={require("../assets/images/common/enter.png")}/>
+            </View>
+        )
     }
 }
 
@@ -48,15 +99,51 @@ const styles = StyleSheet.create({
     container: {
         flex: 1
     },
-    indexBg:{
-        width:Calc.getWidth(750),
-        height:Calc.getHeight(547),
-        position:"absolute",
-        top:0,
-        left:0,
+    //搜索容器
+    searchInfo:{
+        width:Calc.getWidth(702),
+        height:Calc.getHeight(444),
+        backgroundColor:"#fff",
+        marginTop:-(Calc.getHeight(444)/2),
+        marginLeft:"auto",
+        marginRight:"auto",
+        borderRadius:6,
+        elevation:4,
+        shadowColor:'#000',
+        shadowOffset:{h:10,w:10},
+        shadowRadius:3,
+        shadowOpacity:0.5,
+        paddingLeft:Calc.getWidth(20),
+        paddingRight:Calc.getWidth(20),
+        paddingBottom:Calc.getWidth(20)
+    },
+    //View
+    List:{
+        flexDirection:"row",
+        alignItems:"center",
+        height:Calc.getWidth(112),
+        borderBottomWidth:0.5,
+        borderColor:"#d9e1e9",
+        borderStyle:"solid",
+        paddingLeft:Calc.getWidth(10),
+    },
+    //搜索View左侧的图标
+    smallIcon:{
+        width:Calc.getWidth(42),
+        height:Calc.getWidth(42),
+        marginRight:Calc.getWidth(20),
+    },
+    //右侧文字
+    rightText:{
+        fontSize:16,
+        color:"#3a3c3c",
+        marginRight:Calc.getWidth(20),
+    },
+    //右侧图标
+    enterIcon:{
+        width:Calc.getWidth(10),
+        height:Calc.getWidth(18),
     }
-
-
 });
 
 
