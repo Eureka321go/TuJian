4d8519c7156592f5091271f1c923b271a8f9f078
594904020@qq.com <594904020@qq.com>
2017-11-30 00:25:22 +0800

    message

--- components/My.js
+++ components/My.js
@@ -227,6 +227,7 @@ const styles = StyleSheet.create({
     tab:{
         flex:1,
         justifyContent:"center",
+        alignItems:"center",
         position:"relative"
     },
     tabOpWrap:{
