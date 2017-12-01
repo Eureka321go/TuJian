6309ca36d36082433c47d85e2178e9e3de57caf2
594904020@qq.com <594904020@qq.com>
2017-12-01 14:56:31 +0800

    phone

--- components/common/Calc.js
+++ components/common/Calc.js
@@ -6,16 +6,28 @@ import {
 var Dimensions = require('Dimensions');
 const maxW=Dimensions.get("window").width;
 const maxH=Dimensions.get("window").height;
+const fontScale=PixelRatio.getFontScale(); //字体大小所缩放比例
+let pixeRatio=PixelRatio.get(); //获取当前设备的像素密度
+const defaultPixel=2;  //iphone6的像素密度，设计图按iphone6设计
+//px转成dp
+const w2=750/defaultPixel
+const h2=1334/defaultPixel
+const scale=Math.min(maxH/h2,maxW/w2)  //获取缩放比例
+
 
 function getWidth(w) {
-    return w/750*maxW;
+    let newSize=Math.round(w*scale+0.5);
+    return newSize/defaultPixel;
+
 }
 
 function getHeight(h) {
-    return h/1334*maxH;
+    let newSize=Math.round(h*scale+0.5);
+    return newSize/defaultPixel;
 }
 function getFont(size){
-    return PixelRatio.getFontScale(size)
+    let newSize=Math.round((size*scale+0.5)*pixeRatio/fontScale);
+    return newSize/defaultPixel
 }
 export const Calc={
     getWidth,
