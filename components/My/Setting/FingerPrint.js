f099777ad5d6bbc5557867f86ab38587a5100717
594904020@qq.com <594904020@qq.com>
2017-12-13 00:46:53 +0800

    指纹animated

--- components/My/Setting/FingerPrint.js
+++ components/My/Setting/FingerPrint.js
@@ -25,45 +25,57 @@ class FingerPrint extends Component<{}> {
             ani:new Animated.Value(0),
             int:1,//动画执行判断,
         }
+        this.isAni=true;//是否执行动画
     }
     //指纹解锁
     FingerPrint(){
+        let self=this;
         TouchID.authenticate('to demo this react-native component').then(success => {
             // Success code
-            alert("解锁成功")
-        })
-            .catch(error => {
+            //alert("解锁成功")
+            self.props.navigation.goBack()
+
+        }).catch(error => {
                 // Failure code
                 alert("解锁失败")
             });
     }
     startAni(){
-        if(this.state.int==1){
-            this.setState({int:2})
-            Animated.timing(
-                this.state.ani,
-                {
-                    toValue:Calc.getHeight(190),
-                    duration:1500
-                }
-            ).start(()=>{
-                this.startAni()
-            });
-        }else{
-            this.setState({int:1})
-            Animated.timing(
-                this.state.ani,
-                {
-                    toValue:Calc.getHeight(50),
-                    duration:1500
-                }
-            ).start(()=>{
-                this.startAni()
-            });
+        let self=this;
+        if(this.isAni){
+            if(this.state.int==1){
+                this.setState({int:2})
+                Animated.timing(
+                    this.state.ani,
+                    {
+                        toValue:Calc.getHeight(190),
+                        duration:1500
+                    }
+                ).start(()=>{
+                    this.startAni()
+                });
+            }else{
+                this.setState({int:1})
+                Animated.timing(
+                    self.state.ani,
+                    {
+                        toValue:Calc.getHeight(50),
+                        duration:1500
+                    }
+                ).start(()=>{
+                    if(this.isAni){self.startAni()}
+                });
+            }
         }
     }
     componentDidMount(){
         this.startAni();//动画执行
+        setTimeout(()=>{
+            this.FingerPrint();//指纹
+        },500)
+    }
+    componentWillUnmount(){
+        this.isAni=false;//停止动画
     }
     render() {
         return (
