3b970e58f0fd76dcf259b9efcbb92cddb05a1a7a
594904020@qq.com <594904020@qq.com>
2017-12-20 21:31:50 +0800

    客房详情头部

--- components/Hourse/GuestDetail.js
+++ components/Hourse/GuestDetail.js
@@ -11,7 +11,8 @@ import {
     Text,
     View,
     Image,
-    TouchableOpacity
+    TouchableOpacity,
+    ScrollView,
 } from 'react-native';
 import {connect} from "react-redux";
 let commonJS=global.CommonJS;
@@ -45,11 +46,33 @@ class GuestDetail extends Component<{}> {
             headerRight:<HeaderRight/>
         }
     }
+    //头部图片
+    renderTop(){
+        return(
+            <View style={styles.TopWrap}>
+                <TouchableOpacity activeOpacity={1} style={{marginRight:Calc.getWidth(20)}}>
+                    <Image style={styles.TopLeftImg} source={{uri:"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1513785908776&di=18378ddcb1048b85af713edebd9d93bc&imgtype=0&src=http%3A%2F%2Fwww.jswzs.com%2Fueditor%2Fphp%2Fupload%2Fimage%2F20150625%2F1435226368469531.jpg"}}/>
+                </TouchableOpacity>
+                <View style={{flex:1}}>
+                    <TouchableOpacity activeOpacity={1} style={{marginBottom:Calc.getHeight(20)}}>
+                        <Image style={[styles.TopRighgImg]} source={{uri:"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1513785908776&di=18378ddcb1048b85af713edebd9d93bc&imgtype=0&src=http%3A%2F%2Fwww.jswzs.com%2Fueditor%2Fphp%2Fupload%2Fimage%2F20150625%2F1435226368469531.jpg"}}/>
+                    </TouchableOpacity>
+                    <TouchableOpacity activeOpacity={1}>
+                        <Image style={styles.TopRighgImg} source={{uri:"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1513785908776&di=18378ddcb1048b85af713edebd9d93bc&imgtype=0&src=http%3A%2F%2Fwww.jswzs.com%2Fueditor%2Fphp%2Fupload%2Fimage%2F20150625%2F1435226368469531.jpg"}}/>
+                    </TouchableOpacity>
+                    <TouchableOpacity activeOpacity={1} style={{flex:1,alignItems:"center",justifyContent:"center"}}>
+                        <Text style={{fontSize:Calc.getFont(12),color:"#3a3c3c"}}>查看更多</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        )
+    }
     render() {
         return (
-            <View style={styles.container}>
-                <Text>客房详情</Text>
-            </View>
+            <ScrollView style={styles.container} alwaysBounceVertical={false}>
+                {/*头部图片*/}
+                {this.renderTop()}
+            </ScrollView>
         );
     }
 }
@@ -57,14 +80,29 @@ class GuestDetail extends Component<{}> {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
         backgroundColor: '#fff',
+        paddingLeft:Calc.getWidth(24),
+        paddingRight:Calc.getWidth(24),
     },
     collectIcon:{
         width:Calc.getWidth(44),
         height:Calc.getHeight(42)
-    }
+    },
+//    图片展示
+    TopWrap:{
+        height:Calc.getWidth(482),
+        flexDirection:"row",
+    },
+    TopLeftImg:{
+        width:Calc.getWidth(482),
+        height:Calc.getWidth(482),
+        borderRadius:6,
+    },
+    TopRighgImg:{
+        width:Calc.getWidth(200),
+        height:Calc.getWidth(200),
+        borderRadius:6,
+    },
 
 });
 
